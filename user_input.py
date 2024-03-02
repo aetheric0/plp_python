dffeadd4557d186c03370877491b44f4a4478f23
mornark <monarchofaether@gmail.com>
2024-03-02 10:45:02 +0300

    Simple interactive python program

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# A program that asks the user for their name, age and location
+# then prints out a personalized message
+
+name = input("Enter your full name: ")
+age = input("Age: ")
+location = input("Enter your current location: ")
+
+print("Hello", name + " you are", age + " years old and live in", location)
